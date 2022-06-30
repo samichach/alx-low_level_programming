1b4c99d11c5a747cefcaadfc4b8192e35e9df15b
samichach <samuelayana231@gmail.com>
2022-06-30 11:36:56 -0700

    good

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * printf alternative
- *    * Return: Always 0 (Success)
- *     */
+   * main - Entry point
+    * printf alternative
+     * Return: Always 0 (Success)
+      */
 
 int main(void)
 {
